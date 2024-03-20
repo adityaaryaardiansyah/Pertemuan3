1b9ad6e3837be9b4d7711991dd53f8f2dad02b52
adityaaryaardiansyah <xeegaming4@gmail.com>
2024-03-20 19:12:56 +0700

    Pertemuan 3 MOBILE

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(pengirim = "PENDI",
+                        isi = "Telah Menerima Dana Sebesar Rp.50.000",
+                        judul ="Transaksi DANA" )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(pengirim: String, isi: String, judul: String, modifier: Modifier = Modifier) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Blue))
+
+    Column (
+        verticalArrangement = Arrangement.Top
+    ){
+        Text(
+            text =judul,
+            fontSize = 55.sp,
+            textAlign = TextAlign.Center,
+            lineHeight = 80.sp
+
+        )
+    }
+    Column(
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            text =isi,
+            fontSize = 45.sp,
+            textAlign = TextAlign.Center,
+            lineHeight = 45.sp
+        )
+    }
+    Column (
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Text(
+            text =pengirim,
+            fontSize = 50.sp,
+            textAlign = TextAlign.End,
+        )
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        GreetingText(
+            pengirim = "PENDI",
+            isi = "Telah Menerima Dana Sebesar Rp.50.000",
+            judul = "Transaksi DANA"
+        )
+    }
+}
